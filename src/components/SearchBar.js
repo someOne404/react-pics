66134bfccee134d41f0ace52d5a5e9e2d9b6d875
someOne404 <jiayilu96@gmail.com>
2020-02-18 16:03:45 -0500

    arrow function to solve undefined typeerror 2

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = {term: '' };
 
-    onFormSubmit = (event) => {
+    onFormSubmit(event){
         event.preventDefault();
         console.log(this.state.term);
     }
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" 
