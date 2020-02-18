bdf7622945360ed137131333328c5b689f8e5e67
someOne404 <jiayilu96@gmail.com>
2020-02-18 16:15:54 -0500

    pass value from bottom to up

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = {term: '' };
 
-    onFormSubmit(event){
+    onFormSubmit = event => {
         event.preventDefault();
-        console.log(this.state.term);
+        this.props.whenUserSubmits(this.state.term);
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" 
