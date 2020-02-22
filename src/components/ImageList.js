de7aee5ea0109137d590dfd3b2fb74189d7308a4
someOne404 <jiayilu96@gmail.com>
2020-02-22 16:45:32 -0500

    css grid system

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,3 +1,4 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
@@ -5,7 +6,7 @@ const ImageList = (props) => {
         return <img key={image.id} src={image.urls.regular} alt={image.description}/>
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
